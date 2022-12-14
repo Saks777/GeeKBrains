7dbe6b825d57461d07c9d4e331b7fa945d96fc01
Saks777 <sb.sakenyunusov@gmail.com>
2022-12-14 22:51:55 +0500

    11

---
+++ Friend dog/Program.cs
@@ -0,0 +1,27 @@
+﻿int SpeedFriendOne = 1;
+int SpeedFriendTwo = 2;
+int SpeedDog = 5;
+int Distance = 10000;
+int Friend = 1;
+int count = 0;
+int time;
+while (Distance > 10)
+{
+  if (Friend == 1)
+
+  {
+    time = Distance / (SpeedFriendTwo + SpeedDog);
+    Friend = 2;
+    Distance = Distance - (SpeedFriendOne + SpeedFriendTwo) * time;
+    count++;
+  }
+  if (Friend == 2)
+  {
+    time = Distance / (SpeedFriendOne + SpeedDog);
+    Friend = 1;
+    Distance = Distance - (SpeedFriendOne + SpeedFriendTwo) * time;
+    count++;
+  }
+
+}
+Console.WriteLine(count);
