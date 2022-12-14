2584652161d42882f295853abd0846798c492803
Saks777 <sb.sakenyunusov@gmail.com>
2022-12-14 18:59:13 +0500

    7

---
+++ Example0006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Fill in Your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+
+{
+  Console.WriteLine("Hoora,this is Masha !!");
+}
+else
+{
+  Console.Write("Hi, ");
+  Console.WriteLine(username);
+}
