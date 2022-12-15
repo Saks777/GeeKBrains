11ea671d4814bf688a5eb88be8713392e89b377f
Saks777 <sb.sakenyunusov@gmail.com>
2022-12-15 17:37:11 +0500

    4

--- Friends and dog.cs
+++ Friends and dog.cs
@@ -7,20 +7,19 @@ int count = 0;
 int time;
 while(Distance > 10)
 {
-  if(Friend == 1)
-
+  if (Friend == 1)
    {
      time = Distance / (SpeedFriendOne + SpeedDog);
      Friend = 2;
      Distance = Distance - (SpeedFriendOne + SpeedFriendTwo) * time;
-     count++;
+     count = count + 1;
    }
-   if(Friend == 2)
+   else if (Friend == 2)
    {
-    time = Distance / (SpeedFriendOne + SpeedDog);
+    time = Distance / (SpeedFriendTwo + SpeedDog);
      Friend = 1;
      Distance = Distance - (SpeedFriendOne + SpeedFriendTwo) * time;
-     count++;
+     count = count + 1;
    }
 
 }
