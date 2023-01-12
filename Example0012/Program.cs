519621b08447166438dcb74c1aef1ed4d3537db7
Saks777 <sb.sakenyunusov@gmail.com>
2023-01-12 22:00:28 +0500

    Array

---
+++ Example0012/Program.cs
@@ -0,0 +1,92 @@
+﻿
+/*Совмещение массивов и методов.По умолчанию в массиве 10 элементов.Определили массив из 10 элементов, 
+вызвали метод ФилАррэй который заполнил массив.Метод ПринтАррэй распечатал массив в рандомном порядке*/
+
+/*void FillArray(int[] collection)
+{
+
+  int length = collection.Length;
+  int index = 0;
+  while(index < length)
+  {
+    collection[index] = new Random().Next(1, 10);
+    index++;
+  }
+
+}
+
+void PrintArray(int[] col)
+{
+  int count = col.Length;
+  int position = 0;
+  while(position < count)
+  {
+    Console.WriteLine(col[position]);
+    position++;
+  }
+}
+
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);*/
+
+// Новая задача. Адаптировать предыдущую задачу с использованием метода.Если убрать break то будет определять последнюю в очереди позицию числа, если их несколько
+
+void FillArray(int[] collection)
+{
+
+  int length = collection.Length;
+  int index = 0;
+  while(index < length)
+  {
+    collection[index] = new Random().Next(1, 10);
+    index++;
+  }
+
+}
+
+void PrintArray(int[] col)
+{
+  int count = col.Length;
+  int position = 0;
+  while(position < count)
+  {
+    Console.WriteLine(col[position]);
+    position++;
+  }
+}
+
+int IndexOf(int[]   collection, int find)
+{
+  int count = collection.Length;
+  int index = 0;
+  int position = -1;
+
+  while(index < count)
+  {
+    if(collection[index] == find)
+    {
+      position = index;
+      break;
+    }
+    index++;
+  }
+    return position;
+
+}
+
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
+
+
+
+
+
