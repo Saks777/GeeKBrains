795e22527ee3729ac68f83ec20414a6c4b795780
Saks777 <sb.sakenyunusov@gmail.com>
2023-01-12 22:02:35 +0500

    1

---
+++ Example0011/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Имеется одномерный массив array из n элементов, требуется
+найти элемент массива, равный find
+1. Установить счетчик index в позицию 0
+2. Если array [index] = find, алгоритм завершил работу
+успешно.
+3. Увеличить index на 1
+4. Если index < n, то перейти к шагу 2. В противном случае
+алгоритм завершил работу безуспешно.*/
+
+//                         Начало Задачи
+
+/*int [] array = {1, 5, 8, 10, 25, 45, 88, 150 };
+
+int n = array.Length;
+int find = 150;
+
+int index = 0;
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine($" Number of index is:" + index);
+  }
+    index++;
+
+}*/
+
+//              Способ если в массиве 2 одинаковых числа
+
+int [] array = {1, 5, 8, 10, 25, 150, 45, 88, 150 };
+
+int n = array.Length;
+int find = 150;
+
+int index = 0;
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine($" Number of index is:" + index);
+    break;
+  }
+    index++;
+
+}
